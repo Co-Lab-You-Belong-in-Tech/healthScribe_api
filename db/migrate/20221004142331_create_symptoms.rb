400975650efbfea43e617db0a66c5ff53349301d
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-10-04 16:24:09 +0200

    Add disease and symptom models

---
+++ db/migrate/20221004142331_create_symptoms.rb
@@ -0,0 +1,11 @@
+class CreateSymptoms < ActiveRecord::Migration[7.0]
+  def change
+    create_table :symptoms do |t|
+      t.string :name
+      t.text :description
+      t.text :treatment
+
+      t.timestamps
+    end
+  end
+end
